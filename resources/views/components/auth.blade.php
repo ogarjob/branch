a1e8a3c8556f4384d01f8960babdf4357cbfc19d
Ogar <ogarjobbassey@gmail.com>
2023-09-02 17:38:52 +0100

    Create `auth` layout component

---
+++ resources/views/components/auth.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" >
+    <meta charset="utf-8"/>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <title>Welcome - Branch</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>
+    <link href="{{ asset('plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/style.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <script defer src="https://unpkg.com/alpinejs@3.12.0/dist/cdn.min.js"></script>
+    <script src="https://unpkg.com/axios@1.4.0/dist/axios.min.js"></script>
+</head>
+<body  id="kt_body"  class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center" >
+<script>
+    var defaultThemeMode = "light";
+    var themeMode;
+
+    if ( document.documentElement ) {
+        if ( document.documentElement.hasAttribute("data-bs-theme-mode")) {
+            themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+        } else {
+            if ( localStorage.getItem("data-bs-theme") !== null ) {
+                themeMode = localStorage.getItem("data-bs-theme");
+            } else {
+                themeMode = defaultThemeMode;
+            }
+        }
+
+        if (themeMode === "system") {
+            themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        }
+
+        document.documentElement.setAttribute("data-bs-theme", themeMode);
+    }
+</script>
+<!--end::Theme mode setup on page load-->
+<!--begin::Root-->
+<div class="d-flex flex-column flex-root" id="kt_app_root">
+    <!--begin::Page bg image-->
+    <style>
+        body {
+            background-image: url('{{ asset('media/auth/bg10.jpg') }}');
+        }
+
+        [data-bs-theme="dark"] body {
+            background-image: url('{{ asset('media/auth/bg10-dark.jpg') }}');
+        }
+    </style>
+    <!--end::Page bg image-->
+    <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+        <!--begin::Aside-->
+        <div class="d-flex flex-lg-row-fluid">
+            <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+                <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{ asset('media/auth/agency.png') }}" alt=""/>
+                <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{ asset('media/auth/agency-dark.png') }}" alt=""/>
+            </div>
+        </div>
+        <!--begin::Aside-->
+
+        <!--begin::Body-->
+        <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+            <!--begin::Wrapper-->
+            <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+                <!--begin::Content-->
+                <div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+
+                    {{ $slot }}
+
+                </div>
+                <!--end::Content-->
+            </div>
+            <!--end::Wrapper-->
+        </div>
+        <!--end::Body-->
+    </div>
+</div>
+<!--end::Root-->
+
+<!--begin::Javascript-->
+<script>
+<script src="{{ asset('plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('js/scripts.bundle.js') }}"></script>
+<script src="{{ asset("js/request.js") }}"></script>
+
+</body>
+</html>
