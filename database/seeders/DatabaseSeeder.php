9b268a7113ce041f7addae38a417909115fd4663
Ogar <ogarjobbassey@gmail.com>
2023-09-02 15:34:51 +0100

    Create database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Type;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'id'         => 1,
+            'email'      => 'ask4rapzo@gmail.com',
+            'first_name' => 'Ogar',
+            'last_name'  => 'Job',
+        ])->update(['created_by' => 1]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'id'         => 2,
+            'first_name' => 'Joseph',
+            'last_name'  => 'Bassey',
+            'email'      => 'ogarjobbassey@gmail.com',
+            'phone'      => '08126012460',
+        ])->update(['created_by' => 1]);
+
+        User::factory()->create([
+            'id'         => 3,
+            'first_name' => 'Nicholine',
+            'last_name'  => 'Mgbe',
+            'type'       => 'customer',
+            'email'      => 'nickyh@gmail.com',
+            'phone'      => '08126012678',
+        ])->update(['created_by' => 1]);
+
+        User::factory()->create([
+            'id'         => 4,
+            'first_name' => 'Mary',
+            'last_name'  => 'Akpan',
+            'type'       => 'customer',
+            'email'      => 'mary@gmail.com',
+            'phone'      => '08126012679',
+        ])->update(['created_by' => 1]);
+
+        Type::factory()->create([
+            'id'                => 1,
+            'name'              => 'Emergency',
+            'minimum_balance'   => 1000,
+            'interest_rate'     => 10,
+        ]);
+
+        Type::factory()->create([
+            'id'                => 2,
+            'name'              => 'Locked',
+            'minimum_balance'   => 2000,
+            'interest_rate'     => 20,
+        ]);
     }
 }
