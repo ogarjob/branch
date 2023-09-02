f8c249b20eba958ebd0095a1b0dc234ff055014c
Ogar <ogarjobbassey@gmail.com>
2023-09-02 15:20:10 +0100

    Update `User` model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,53 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Concerns\ChecksUserState;
+use App\Models\Concerns\ObservesWrites;
+use App\Models\Enums\Gender;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    use HasFactory, HasApiTokens, Notifiable, SoftDeletes, ChecksUserState, ObservesWrites;
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token', 'meta'];
 
     /**
      * The attributes that should be cast.
-     *
-     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'last_login'        => 'datetime',
+        'banned_until'      => 'datetime',
+        'gender'            => Gender::class,
+        'password'          => 'hashed',
     ];
+
+    public function name(): Attribute
+    {
+        return Attribute::get(fn () => Str::title(
+            $this->first_name.' '.$this->last_name
+        ));
+    }
+
+    public function photo(): Attribute
+    {
+        return Attribute::get(fn () => "https://ui-avatars.com/api?background=f8f5ff&color=7239ea&name={$this->name}&format=svg");
+    }
+
+    public function wallets(): HasMany
+    {
+        return $this->HasMany(Wallet::class);
+    }
 }
